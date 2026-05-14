2aa67359daa404ccb65cedbb3441f063d8440246
Artspessoto <artspessoto@gmail.com>
2026-05-13 22:11:09 -0300

    chore: install pino-pretty for better dev logging

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from "fastify";
+import { env } from "./env";
+import { errorHandler } from "./http/error-handler";
+import { usersRoutes } from "./http/controllers/routes";
 
-export const app = fastify();
+export const app = fastify({
+  logger: {
+    transport: env.NODE_ENV == "dev" ? { target: "pino-pretty" } : undefined,
+  },
+});
+
+app.register(usersRoutes);
+
+app.setErrorHandler(errorHandler);
